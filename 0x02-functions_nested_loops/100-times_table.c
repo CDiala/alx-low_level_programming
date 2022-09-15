1d2d2e5754d9749998a0331b9a080bc2152c6b0e
Chibuzo Diala <dialachibuzo@yahoo.com>
2022-09-14 20:46:16 -0700

    corrections

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -35,14 +35,16 @@ _putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(n + '0');
-} else if (n > 9 && n <= 99)
+} 
+else if (n > 9 && n <= 99)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(n / 10 + '0');
 _putchar(n % 10 + '0');
-} else
+} 
+else
 {
 _putchar(',');
 _putchar(' ');
