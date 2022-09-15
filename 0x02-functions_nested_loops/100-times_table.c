ab3dfbfe96ad7101603bab2026169529858caf27
Chibuzo Diala <dialachibuzo@yahoo.com>
2022-09-14 20:58:17 -0700

    corrections

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,10 +14,8 @@ for (i = 0; i <= n; i++)
 {
 _putchar('0');
 for (j = 1; j <= n; j++)
-{																	
-putformat(i * j);
-}																	
-_putchar('\n');
+{																	putformat(i * j);
+}																	_putchar('\n');
 }
 }
 }
@@ -31,11 +29,11 @@ void putformat(int n)
 if (n <= 9)
 {
 _putchar(',');
-_putchar(' ');	
+_putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(n + '0');
-} 
+}
 else if (n > 9 && n <= 99)
 {
 _putchar(',');
@@ -43,7 +41,7 @@ _putchar(' ');
 _putchar(' ');
 _putchar(n / 10 + '0');
 _putchar(n % 10 + '0');
-} 
+}
 else
 {
 _putchar(',');
