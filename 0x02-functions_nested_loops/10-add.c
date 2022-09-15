9b4861d6b40cd621d97fc9a553e86e50bb1da995
Chibuzo Diala <dialachibuzo@yahoo.com>
2022-09-14 17:17:22 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Check Holberton
+ * @x: An integer x
+ * @y: An integer y
+ * Description: Function that adds two integers and returns the result
+ * Return: Result to add a and b
+ */
+int add(int x, int y)
+{
+return (x + y);
+}
