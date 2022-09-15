eaed08fe5a0308ce87e7ddb769f33c7fe8b9d570
Chibuzo Diala <dialachibuzo@yahoo.com>
2022-09-14 21:12:07 -0700

    corrections

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,53 +1,40 @@
 #include "main.h"
 
 /**
- * print_times_table - prints times table for numbers from 0-14
- * @n: An input integer value
- * Return: Nothing
+ * print_times_table - prints the  times table with
+ * parameter
+ * @x: parameter
+ * Return: returns nothing
  */
-void print_times_table(int n)
+void print_times_table(int x)
 {
-int i, j;
-if (n > 0 && n < 15)
+int digit, mult, result;
+if (x <= 15 && x >= 0)
 {
-for (i = 0; i <= n; i++)
+for (digit = 0; digit <= x; digit++)
 {
 _putchar('0');
-for (j = 1; j <= n; j++)
-{																	putformat(i * j);
-}																	_putchar('\n');
-}
-}
-}
-/**
- * putformat - formatted characters to output
- * @n: number to format
- * Return: nothing
- */
-void putformat(int n)
-{
-if (n <= 9)
+for (mult = 1; mult <= x; mult++)
 {
 _putchar(',');
 _putchar(' ');
+result = digit * mult;
+if (result <= 99)
 _putchar(' ');
+if (result <= 9)
 _putchar(' ');
-_putchar(n + '0');
-}
-else if (n > 9 && n <= 99)
+if (result >= 100)
 {
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(n / 10 + '0');
-_putchar(n % 10 + '0');
+_putchar((result / 100) + '0');
+_putchar((result / 10) % 10 + '0');
 }
-else
+else if (result <= 99 && result >= 10)
 {
-_putchar(',');
-_putchar(' ');
-_putchar(n / 100 + '0');
-_putchar(n / 10 % 10 + '0');
-_putchar(n % 10 + '0');
+_putchar((result / 10) + '0');
+}
+_putchar((result % 10) + '0');
+}
+_putchar('\n');
+}
 }
 }
