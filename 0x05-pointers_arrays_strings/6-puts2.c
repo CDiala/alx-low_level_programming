0a4004000db1a62d4c96d8316d9f807e21d35f81
Chibuzo Diala <dialachibuzo@yahoo.com>
2022-09-19 19:15:56 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+  
 /**
  * puts2 - a function that prints every other character of a string,
  * starting with the first character, followed by a new line.
@@ -8,15 +8,15 @@
  */
 void puts2(char *str)
 {
-	int x  = 0, i = 0;
-
-	while (str[x] != '\0')
-		x++;
-
-	x -= 1;
-
-	for (; i <= x; i += 2)
-		_putchar(str[i]);
-
-	_putchar('\n');
+int x = 0, i = 0;
+while (str[x] != '\0')
+{
+x++;
+}
+x -= 1;
+for (; i <= x; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
